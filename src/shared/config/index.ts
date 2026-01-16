2b2eaec753db11e04e5202696476d7d6f784f894
seoeongeueun <erin9901@gmail.com>
2026-01-16 18:17:39 +0900

    chore: 공용 api request helper 추가
    - api request를 json으로 파싱하고 에러를 핸들링하는 공용 헬퍼

---
+++ src/shared/config/index.ts
@@ -0,0 +1,2 @@
+//추후 다른 설정이 더 추가될 경우를 대비해 index.ts로 통합 관리
+export { WEATHER_BASE_URL } from "./weather";
---
+++ src/shared/config/weather.ts
@@ -0,0 +1,1 @@
+export const WEATHER_BASE_URL = "https://api.open-meteo.com"; //날씨를 가져올 출처 open meteo
