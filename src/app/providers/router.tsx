5f2fc791ba90df59f720f02d42c126ec0174a315
seoeongeueun <erin9901@gmail.com>
2026-01-21 00:32:17 +0900

    refactor: 카카오맵을 공용 레이아웃으로 이동
    -페이지 이동간 재렌더링 방지
    -/info인지 분기로 디스플레이 여부 결정

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,14 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import { routes } from "@/shared/config/routes";
 import { InfoPage } from "@/pages/info";
 import { MainPage } from "@/pages/main";
+import { KakaoMap } from "@/widgets/kakao-maps";
+import { Header } from "@/widgets/dashboard/ui/Header";
+
+function Layout() {
+  const location = useLocation();
+  const isInfoPage = location.pathname.startsWith("/info");
+
+  return (
+    <div
+      className={`${isInfoPage ? "max-w-[1000px]" : "max-w-[1800px]"} h-full w-full flex flex-col justify-self-center`}
+    >
+      <Header />
+      <div className="flex flex-col md:flex-row w-full h-full md:h-[calc(100%-80px)]">
+        <section
+          className={`p-4 md:p-6 lg:p-10 md:pt-2 lg:pt-4 w-full md:w-1/2 h-1/2 shrink-0 md:h-full ${isInfoPage ? "hidden" : ""}`}
+        >
+          <KakaoMap />
+        </section>
+        <section
+          className={`${isInfoPage ? "w-full" : "w-full md:w-1/2"} h-full px-4 md:px-0`}
+        >
+          <Outlet />
+        </section>
+      </div>
+    </div>
+  );
+}
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={routes.home} element={<MainPage />} />
-        <Route path={routes.info} element={<InfoPage />} />{" "}
+        <Route element={<Layout />}>
+          <Route path={routes.home} element={<MainPage />} />
+          <Route path={routes.info} element={<InfoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,22 +1,5 @@
-import { KakaoMap } from "@/widgets/kakao-maps";
 import { Dashboard } from "@/widgets/dashboard";
-//import { usePrefetchCitiesWeather } from "@/features/prefetch-cities-weather";
-import { Header } from "@/widgets/dashboard/ui/Header";
 
 export function MainPage() {
-  //usePrefetchCitiesWeather();
-
-  return (
-    <div className="max-w-[1800px] h-full w-full flex flex-col justify-self-center">
-      <Header />
-      <div className="flex flex-col md:flex-row w-full h-full md:h-[calc(100%-80px)]">
-        <section className="p-4 md:p-6 lg:p-10 md:pt-2 lg:pt-4 w-full md:w-1/2 h-1/2 shrink-0 md:h-full">
-          <KakaoMap />
-        </section>
-        <section className="w-full md:w-1/2 h-full px-4 md:px-0">
-          <Dashboard />
-        </section>
-      </div>
-    </div>
-  );
+  return <Dashboard />;
 }
