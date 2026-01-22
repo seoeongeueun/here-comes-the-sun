82779c36c849ded7be114052dabd15aa8b122c83
seoeongeueun <erin9901@gmail.com>
2026-01-22 22:23:24 +0900

    style: 헤더 로고 이미지 변경

--- src/widgets/dashboard/ui/Header.tsx
+++ src/widgets/dashboard/ui/Header.tsx
@@ -1,10 +1,10 @@
+import { WeatherImage } from "@/shared/ui";
+
 export function Header() {
   return (
     <header className="text-md md:text-lg h-fit md:h-[80px] px-4 mt-2 md:mt-0 md:px-6 lg:px-10 text-white ml-auto font-bold flex flex-row items-center gap-1">
       Here Comes the Sun
-      <span aria-hidden className="text-lg mb-1">
-        🌤️
-      </span>
+      <WeatherImage weather="partly_cloudy" size="medium" />
     </header>
   );
 }
